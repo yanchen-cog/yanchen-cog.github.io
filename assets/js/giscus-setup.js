aebf90ff9c16f6c483ace936673b7c915453a2b9
yanchen-cog <yanchen-cog@users.noreply.github.com>
2025-11-10 21:26:15 +0000

    Deploying to gh-pages from @ yanchen-cog/yanchen-cog.github.io@84a1ab25bab173e5cfed4cacf06729b645abe6b7 🚀

---
+++ assets/js/giscus-setup.js
@@ -0,0 +1,33 @@
+function determineGiscusTheme() {
+  
+    return "";
+  
+}
+
+(function setupGiscus() {
+  let giscusTheme = determineGiscusTheme();
+
+  let giscusAttributes = {
+    src: "https://giscus.app/client.js",
+    "data-repo": "yanchen-cog/yanchen-cog.github.io",
+    "data-repo-id": "",
+    "data-category": "",
+    "data-category-id": "",
+    "data-mapping": "",
+    "data-strict": "",
+    "data-reactions-enabled": "",
+    "data-emit-metadata": "",
+    "data-input-position": "",
+    "data-theme": giscusTheme,
+    "data-lang": "",
+    crossorigin: "anonymous",
+    async: true,
+  };
+
+  let giscusScript = document.createElement("script");
+  Object.entries(giscusAttributes).forEach(([key, value]) =>
+    giscusScript.setAttribute(key, value)
+  );
+  document.getElementById("giscus_thread").appendChild(giscusScript);
+})();
+
